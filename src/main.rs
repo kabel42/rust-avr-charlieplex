1622acb70a1e1408d290ec58a8e320821faedb03
Jannis Konrad <kabel42@gmail.com>
2023-05-01 23:41:58 +0200

    fade

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,17 @@ use arduino_hal::port::mode::{Floating, Input};
 use arduino_hal::port::Pin;
 use panic_halt as _;
 
-enum Led {
-    Led1,
-    Led2,
-    Led3,
-    Led4,
-    Led5,
-    Led6,
+fn delay() {
+    arduino_hal::delay_us(10);
 }
 
-fn led_on(
+fn _led_on(
     b: Pin<Input<Floating>>,
     c: Pin<Input<Floating>>,
 ) -> (Pin<Input<Floating>>, Pin<Input<Floating>>) {
     let _b = b.into_output_high();
     let _c = c.into_output();
-    arduino_hal::delay_ms(100);
+    delay();
     return (_b.into_floating_input(), _c.into_floating_input());
 }
 
@@ -33,34 +28,63 @@ fn main() -> ! {
     let mut pin3 = pins.d3.downgrade();
     let mut pin4 = pins.d4.downgrade();
 
-    let mut i = Led::Led1;
+    let mut i: usize = 0;
+    let mut j: i8 = 0;
+    let mut data: [i8; 6] = [0; 6];
+    let mut dir: i8 = 1;
+    let mut cnt: u8 = 0;
+    let min = -3;
+    let max = 4;
 
     loop {
-        match i {
-            Led::Led1 => {
-                (pin3, pin2) = led_on(pin3, pin2);
-                i = Led::Led2;
+        if data[i] > j {
+            match i {
+                0 => {
+                    (pin3, pin2) = _led_on(pin3, pin2);
+                }
+                1 => {
+                    (pin2, pin3) = _led_on(pin2, pin3);
+                }
+                2 => {
+                    (pin4, pin3) = _led_on(pin4, pin3);
+                }
+                3 => {
+                    (pin3, pin4) = _led_on(pin3, pin4);
+                }
+                4 => {
+                    (pin2, pin4) = _led_on(pin2, pin4);
+                }
+                5 => {
+                    (pin4, pin2) = _led_on(pin4, pin2);
+                }
+                _ => {}
+            };
+          } else {
+            delay();
+          }
+          i += 1;
+
+          if i == 6
+          {
+            i = 0;
+            j += 1;
+            if j == max
+            {
+              j = 0;
+              cnt += 1;
+              if cnt == 150
+              {
+                cnt = 0;
+                data[5] = data[4];
+                data[4] = data[3];
+                data[3] = data[2];
+                data[2] = data[1];
+                data[1] = data[0];
+                data[0] += dir;
+                if data[0] == max {dir = -1;}
+                if data[0] == min {dir = 1;}
+              }
             }
-            Led::Led2 => {
-                (pin2, pin3) = led_on(pin2, pin3);
-                i = Led::Led3;
-            }
-            Led::Led3 => {
-                (pin4, pin3) = led_on(pin4, pin3);
-                i = Led::Led4;
-            }
-            Led::Led4 => {
-                (pin3, pin4) = led_on(pin3, pin4);
-                i = Led::Led5;
-            }
-            Led::Led5 => {
-                (pin2, pin4) = led_on(pin2, pin4);
-                i = Led::Led6;
-            }
-            Led::Led6 => {
-                (pin4, pin2) = led_on(pin4, pin2);
-                i = Led::Led1;
-            }
-        }
+          }
     }
 }
