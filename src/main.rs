99e894afd486112600edc35350be2baa5cfc2b74
Jannis Konrad <kabel42@gmail.com>
2023-05-01 22:33:17 +0200

    function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+use arduino_hal::port::mode::{Floating, Input};
+use arduino_hal::port::Pin;
 use panic_halt as _;
 
 enum Led {
@@ -12,93 +14,52 @@ enum Led {
     Led6,
 }
 
+fn led_on(
+    b: Pin<Input<Floating>>,
+    c: Pin<Input<Floating>>,
+) -> (Pin<Input<Floating>>, Pin<Input<Floating>>) {
+    let _b = b.into_output_high();
+    let _c = c.into_output();
+    arduino_hal::delay_ms(100);
+    return (_b.into_floating_input(), _c.into_floating_input());
+}
+
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
-
-    let mut pin2 = pins.d2;
-    let mut pin3 = pins.d3;
-    let mut pin4 = pins.d4;
+    let mut pin2 = pins.d2.downgrade();
+    let mut pin3 = pins.d3.downgrade();
+    let mut pin4 = pins.d4.downgrade();
 
     let mut i = Led::Led1;
 
     loop {
         match i {
             Led::Led1 => {
-                let t4 = pin4.into_opendrain();
-                let t3 = pin3.into_output_high();
-                let t2 = pin2.into_output();
-                arduino_hal::delay_ms(1);
-                pin2 = t2.into_floating_input();
-                pin3 = t3.into_floating_input();
-                pin4 = t4.into_floating_input();
+                (pin3, pin2) = led_on(pin3, pin2);
                 i = Led::Led2;
             }
             Led::Led2 => {
-                let t4 = pin4.into_opendrain();
-                let t3 = pin3.into_output();
-                let t2 = pin2.into_output_high();
-                arduino_hal::delay_ms(1);
-                pin2 = t2.into_floating_input();
-                pin3 = t3.into_floating_input();
-                pin4 = t4.into_floating_input();
+                (pin2, pin3) = led_on(pin2, pin3);
                 i = Led::Led3;
-
             }
             Led::Led3 => {
-                let t2 = pin2.into_opendrain();
-                let t3 = pin3.into_output();
-                let t4 = pin4.into_output_high();
-                arduino_hal::delay_ms(1);
-                pin2 = t2.into_floating_input();
-                pin3 = t3.into_floating_input();
-                pin4 = t4.into_floating_input();
+                (pin4, pin3) = led_on(pin4, pin3);
                 i = Led::Led4;
-
             }
             Led::Led4 => {
-                let t2 = pin2.into_opendrain();
-                let t3 = pin3.into_output_high();
-                let t4 = pin4.into_output();
-                arduino_hal::delay_ms(1);
-                pin2 = t2.into_floating_input();
-                pin3 = t3.into_floating_input();
-                pin4 = t4.into_floating_input();
+                (pin3, pin4) = led_on(pin3, pin4);
                 i = Led::Led5;
-
             }
             Led::Led5 => {
-                let t3 = pin3.into_opendrain();
-                let t2 = pin2.into_output_high();
-                let t4 = pin4.into_output();
-                arduino_hal::delay_ms(1);
-                pin2 = t2.into_floating_input();
-                pin3 = t3.into_floating_input();
-                pin4 = t4.into_floating_input();
+                (pin2, pin4) = led_on(pin2, pin4);
                 i = Led::Led6;
-
             }
             Led::Led6 => {
-                let t3 = pin3.into_opendrain();
-                let t2 = pin2.into_output();
-                let t4 = pin4.into_output_high();
-                arduino_hal::delay_ms(1);
-                pin2 = t2.into_floating_input();
-                pin3 = t3.into_floating_input();
-                pin4 = t4.into_floating_input();
+                (pin4, pin2) = led_on(pin4, pin2);
                 i = Led::Led1;
-
             }
         }
     }
